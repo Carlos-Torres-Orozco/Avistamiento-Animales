72008fd3bbf5737c72360898e25457482638c765
Carlos-Torres-Orozco <carlos.torres9858@gmail.com>
2025-10-20 23:37:27 -0600

    Primer commit desde Visual Studio

---
+++ src/main/java/com/avistamientos_animales/demo/controller/PageController.java
@@ -0,0 +1,42 @@
+package com.avistamientos_animales.demo.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "index";  // Carga templates/index.html
+    }
+
+    @GetMapping("/gestion")
+    public String gestion() {
+        return "gestion";  // Carga templates/gestion.html
+    }
+
+    @GetMapping("/especies")
+    public String especies() {
+        return "especies";  // Carga templates/especies.html
+    }
+
+    @GetMapping("/animales")
+    public String animales() {
+        return "animales";  // Carga templates/animales.html
+    }
+
+    @GetMapping("/avistamientos")
+    public String avistamientos() {
+        return "avistamientos";  // Carga templates/avistamientos.html
+    }
+
+    @GetMapping("/ubicaciones")
+    public String ubicaciones() {
+        return "ubicaciones";  // Carga templates/ubicaciones.html
+    }
+
+    @GetMapping("/observador")
+    public String observadores() {
+        return "ubicaciones";  // Carga templates/ubicaciones.html
+    }
+}
